5ae90b1a0de454f0aa8d499a5bfc96b6f7b49f13
Mason Hughes <masonsh98@gmail.com>
2021-01-31 23:07:56 -0600

    Initial Commit

---
+++ flixster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  flixster
+//
+//  Created by Mason Hughes on 1/27/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
